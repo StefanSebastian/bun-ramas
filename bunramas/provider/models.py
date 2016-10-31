5f615477a61b241861a6e4a912b91e4e69ac5f31
StefanSebastian <stefansebii@gmail.com>
2016-10-31 16:41:31 +0200

    Added Offer model

--- bunramas/provider/models.py
+++ bunramas/provider/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+'''
+Offer model
+Describes an offer made by a provider
+'''
+class Offer(models.Model):
+	'''
+	Title - CharField
+	The title of the offer, visible in offer list
+	'''
+	title = models.CharField(max_length = 250);
+
+	'''
+	Description - TextField
+	Contains the offer's details
+	'''
+	description = models.TextField();
+
+	'''
+	Expration Date - DateTimeField
+	The offer's expiration date
+	'''
+	expiration_date = models.DateTimeField();
+
+	'''
+	Location - CharField
+	Location of the provider
+	'''
+	location = models.CharField(max_length = 250);
+
+	'''
+	Will contain reference to provider
+	'''
+	provider = models.CharField(max_length = 250);
