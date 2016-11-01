0b5a60cee5f6d75954f523aee180969f4a5551bb
2dor <ciurcat@gmail.com>
2016-11-01 18:14:35 +0000

    Move OfferListView to the consumer app

--- bunramas/home/urls.py
+++ bunramas/home/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 # from . import views
-from home.views import OfferListView
+from consumer.views import OfferListView
 
 urlpatterns = [
     url(r'^$', OfferListView.as_view(), name='offer-list'),
