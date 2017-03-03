4c94967d1f33657abb685523274a4aa798ee56b1
StefanSebastian <stefansebii@gmail.com>
2017-03-03 18:37:00 +0200

    Added Consumer user model

--- bunramas/consumer/models.py
+++ bunramas/consumer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+Consumer user model
+Describes a consumer user profile
+'''
+class Consumer(models.Model):
+	'''
+	Reference
+	'''
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	'''
+	String representation of the object
+	Returns the user name
+	'''
+	def __str__(self):
+		return self.user.username;
